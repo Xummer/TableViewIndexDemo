620386b734d75a54e8c524b33c489633d1cc55ce
xummer <willx@gobeta.com.cn>
2014-04-25 16:56:15 +0800

    Initial Commit

---
+++ TableIndexDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableIndexDemo
+//
+//  Created by Xummer on 14-4-25.
+//  Copyright (c) 2014年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableIndexDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableIndexDemo
+//
+//  Created by Xummer on 14-4-25.
+//  Copyright (c) 2014年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
